aba0a3c1f0c9c19c0a0fed9cd4db1532167b28e9
PrushilPatel <104359606+PrushilPatel@users.noreply.github.com>
2022-11-04 19:42:43 +0000

    2 more variables added and an array of objects.
    2 more variables added to display game grid and the start button.
    
    Also created an array of objects to hold clubs. both their name and image.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,143 @@
 document.addEventListener("DOMContentLoaded", () => {
   const startGame = document.getElementById("startgame");
+  const gameLevel = document.getElementById("gameLevel");
+  const grid = document.querySelector("#grid");
+
+  const clubs = [
+    {
+      name: "Arsenal",
+      img: "assets/images/arsenal.png",
+    },
+    {
+      name: "Arsenal",
+      img: "assets/images/arsenal.png",
+    },
+    {
+      name: "Atletico",
+      img: "assets/images/atletico.png",
+    },
+    {
+      name: "Atletico",
+      img: "assets/images/atletico.png",
+    },
+    {
+      name: "Bayern",
+      img: "assets/images/bayern.png",
+    },
+    {
+      name: "Bayern",
+      img: "assets/images/bayern.png",
+    },
+    {
+      name: "Barcelona",
+      img: "assets/images/barcelona.png",
+    },
+    {
+      name: "Barcelona",
+      img: "assets/images/barcelona.png",
+    },
+    {
+      name: "Chelsea",
+      img: "assets/images/chelsea.png",
+    },
+    {
+      name: "Chelsea",
+      img: "assets/images/chelsea.png",
+    },
+    {
+      name: "Dortmund",
+      img: "assets/images/dortmund.png",
+    },
+    {
+      name: "Dortmund",
+      img: "assets/images/dortmund.png",
+    },
+    {
+      name: "Inter",
+      img: "assets/images/inter.png",
+    },
+    {
+      name: "Inter",
+      img: "assets/images/inter.png",
+    },
+    {
+      name: "Liverpool",
+      img: "assets/images/liverpool.png",
+    },
+    {
+      name: "Liverpool",
+      img: "assets/images/liverpool.png",
+    },
+    {
+      name: "Lyon",
+      img: "assets/images/lyon.png",
+    },
+    {
+      name: "Lyon",
+      img: "assets/images/lyon.png",
+    },
+    {
+      name: "Mancity",
+      img: "assets/images/mancity.png",
+    },
+    {
+      name: "Mancity",
+      img: "assets/images/mancity.png",
+    },
+    {
+      name: "Manutd",
+      img: "assets/images/manutd.png",
+    },
+    {
+      name: "Manutd",
+      img: "assets/images/manutd.png",
+    },
+    {
+      name: "Newcastle",
+      img: "assets/images/newcastle.png",
+    },
+    {
+      name: "Newcastle",
+      img: "assets/images/newcastle.png",
+    },
+    {
+      name: "PSG",
+      img: "assets/images/psg.png",
+    },
+    {
+      name: "PSG",
+      img: "assets/images/psg.png",
+    },
+    {
+      name: "Realmadrid",
+      img: "assets/images/realmadrid.png",
+    },
+    {
+      name: "Realmadrid",
+      img: "assets/images/realmadrid.png",
+    },
+    {
+      name: "Spurs",
+      img: "assets/images/spurs.png",
+    },
+    {
+      name: "Spurs",
+      img: "assets/images/spurs.png",
+    },
+    {
+      name: "Valencia",
+      img: "assets/images/valencia.png",
+    },
+    {
+      name: "Valencia",
+      img: "assets/images/valencia.png",
+    },
+  ];
+
+
+
+
+
+
+
 });
